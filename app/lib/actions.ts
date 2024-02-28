5d6a3e70885eb4c086856bafac6ee5201bffb560
javierBorjas <cristman11@gmail.com>
2024-02-28 02:03:56 -0300

    Pasar la id accion al servidor

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -20,7 +20,9 @@ export async function createInvoice(formData: FormData) {
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
+        
     });
+
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -31,5 +33,31 @@ export async function createInvoice(formData: FormData) {
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+
+    
+}
+
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+// ...
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
 
